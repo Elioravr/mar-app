0925f654c462135c3302483dbe686839f5855e8a
Elior Avramoviz <elioravr@gmail.com>
2020-03-18 14:23:16 +0200

    Add new page for creating new meal

--- src/App.js
+++ src/App.js
@@ -1,42 +1,81 @@
 import React, {useState} from 'react';
 import './App.scss';
 
+const MEALS_LIST_PAGE = 'mealsListPage';
+const NEW_MEAL_PAGE = 'newMealPage';
+
+const navbarProps = {
+  [MEALS_LIST_PAGE]: {
+    title: 'Mami I\'m Hungry',
+    shouldHaveProfile: true
+  },
+  [NEW_MEAL_PAGE]: {
+    title: 'New Meal?! 😱',
+    shouldHaveCancelButton: true
+  }
+}
+
 function App() {
+  const [currentPage, setCurrentPage] = useState(MEALS_LIST_PAGE);
+
+  const moveToNewMealPage = () => setCurrentPage(NEW_MEAL_PAGE);
+  const moveToMealsListPage = () => setCurrentPage(MEALS_LIST_PAGE);
+
   return (
     <div className="App">
-      <MealsListPage />
+      <Navbar {...navbarProps[currentPage]} moveToMealsListPage={moveToMealsListPage} />
+      <MealsListPage moveToNewMealPage={moveToNewMealPage} isCurrentPage={currentPage === MEALS_LIST_PAGE} />
+      <NewMealPage isCurrentPage={currentPage === NEW_MEAL_PAGE} />
     </div>
   );
 }
 
-const MealsListPage = () => {
+const NewMealPage = ({isCurrentPage}) => {
   return (
-    <>
-      <Navbar title="Mami I'm Hungry" shouldHaveProfile={true} />
-      <AddMealButton />
-
-      <MealItem />
-      <MealItem />
-      <MealItem />
-      <MealItem />
-      <MealItem />
-      <MealItem />
-      <MealItem />
-    </>
+    <Page isCurrentPage={isCurrentPage}>
+      create new meal
+    </Page>
   );
 }
 
-const Navbar = ({title, shouldHaveProfile}) => {
+const MealsListPage = ({moveToNewMealPage, isCurrentPage}) => {
+  return (
+    <Page isCurrentPage={isCurrentPage}>
+      <AddMealButton onClick={moveToNewMealPage} />
+
+      <MealItem />
+      <MealItem />
+      <MealItem />
+      <MealItem />
+      <MealItem />
+      <MealItem />
+      <MealItem />
+    </Page>
+  );
+}
+
+const Page = ({children, isCurrentPage}) => {
+  return (
+    <div className={`page ${isCurrentPage ? 'opened' : 'closed'}`}>
+      {children}
+    </div>
+  );
+}
+
+const Navbar = ({title, shouldHaveProfile, shouldHaveCancelButton, moveToMealsListPage}) => {
     const [isProfileOpen, setIsProfileOpen] = useState(false);
     const profileClassName = `profile ${isProfileOpen ? 'wide' : ''}`;
 
-    const handleProfileClick = () => setIsProfileOpen(!isProfileOpen)
+    const handleProfileClick = () => setIsProfileOpen(!isProfileOpen);
 
     return (
       <div className="navbar">
         <span>{title}</span>
         {shouldHaveProfile && isProfileOpen && <div className="backdrop"></div>}
         {shouldHaveProfile && <div className={profileClassName} onClick={handleProfileClick}></div>}
+        {shouldHaveCancelButton &&
+          <div className="close-button" onClick={moveToMealsListPage}></div>
+        }
       </div>
     )
 }
@@ -53,9 +92,9 @@ const MealItem = () => {
   );
 }
 
-const AddMealButton = () => {
+const AddMealButton = ({onClick}) => {
   return (
-    <div className="add-meal-button">
+    <div className="add-meal-button" onClick={onClick}>
       <div className="background"></div>
     </div>
   );
